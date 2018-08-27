48ed3e8fabca9d93f0180318d4b567691124c22c
qzcsczw <qzcsczw@163.com>
2018-08-27 10:07:00 +0800

    20180827 transfer.js

--- pages/order/transfer/transfer.js
+++ pages/order/transfer/transfer.js
@@ -377,7 +377,8 @@ Page({
       var shareObj = {
         title: title,        // 默认是小程序的名称(可以写slogan等)
         desc:"礼物代表我的心意",
-        path: '/pages/order/receive/receive?order_no=' + order_no + '&receive=1',   // 默认是当前页面，必须是以‘/’开头的完整路径
+        //path: '/pages/order/receive/receive?order_no=' + order_no + '&receive=1',   // 默认是当前页面，必须是以‘/’开头的完整路径
+        path : '/pages/hall/hall?page_type=2&order_no=' + order_no + '&receive=1',
         imageUrl: imageUrl,     //自定义图片路径，可以是本地文件路径、代码包文件路径或者网络图片路径，支持PNG及JPG，不传入 imageUrl 则使用默认截图。显示图片长宽比是 5:4
       　success: function (res) {　　　
           console.log(res)
@@ -441,7 +442,8 @@ Page({
           icon: 'loading',
           duration: 1500
         })  
-        shareObj.path = '/pages/order/receive/receive?order_no=' + order_no+'&receive=1'
+        //shareObj.path = '/pages/order/receive/receive?order_no=' + order_no+'&receive=1'
+        shareObj.path = '/pages/hall/hall?page_type=2&order_no=' + order_no + '&receive=1'
         shareObj.imageUrl = imageUrl
         console.log('礼物分享:')
         console.log(shareObj)
