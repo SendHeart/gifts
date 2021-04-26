a82058d4799d921a14c484e36e011ae940fc82c5
necoer666 <42019741+necoer666@users.noreply.github.com>
2021-04-26 13:43:10 +0800

    20210426 UI调整
    20210426 UI调整

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -66,6 +66,12 @@ Page({
     is_buymyself:1,
     goodsshape:1,
   },
+  goBack: function () {
+      wx.switchTab({
+        url: '../hall/hall'
+      })
+  },
+  
   bindTextAreaBlur: function (e) {
     var that = this;
     that.setData({
