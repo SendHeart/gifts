944380de3b28e6f8a5b830aa1de5cb0e7bc180bd
qzcsczw <qzcsczw@163.com>
2021-05-07 22:22:48 +0800

    20210507

--- pages/my/index.js
+++ pages/my/index.js
@@ -1714,7 +1714,7 @@ Page({
   login: function () { 
     var that = this
     wx.navigateTo({
-      url: '/pages/login/login?frompage=/pages/index'
+      url: '/pages/login/login?frompage=/pages/my/index'
     })
   },
 
